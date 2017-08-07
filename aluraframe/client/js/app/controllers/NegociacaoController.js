a369d5bdc513ec0f47366ffd4bdaf7bae91dfb85
gabriel949 <gabriel.costa949@gmail.com>
2017-08-07 19:27:27 -0300

    Aula 4

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,19 +2,22 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
-
-
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event) {
+
         event.preventDefault();
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        let helper = new DateHelper();
 
+        let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.valor
+    );
+console.log(negociacao.data);
     }
 }
